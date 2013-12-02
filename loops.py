7e0b6af6a91b34f029e1fd6c193c1e0f529117bf
mdkallick <need4huskys@gmail.com>
2013-12-02 14:56:43 -0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -6,3 +6,5 @@ while x > 0:
  print x
  x = x - 1
 print "done"
+
+# You can try to mess with this to make it do different things, like print the numbers from 5 to 1, or print text instead of numbers.
