20e0770fee18afc21b609f314bfcb94992540d57
mdkallick <need4huskys@gmail.com>
2013-12-02 14:52:31 -0500

    Update stringsandintegers.py

--- stringsandintegers.py
+++ stringsandintegers.py
@@ -1,4 +1,6 @@
 ## Written by Mathias Dyssegaard Kallick	11/16/13
-## Introduction to strings, integers, and floating-point numbers
+## Introduction to strings, integers, and floating-point integers
 
-#
+# Strings and integers are very simple, but will make up most of the variables that you create. An integer is exactly the same as an integer in math: a number, without any decimal points after it. The general output of python math equations will be an integer, rounded down.
+# Strings are simply anything that you surround by quotation marks. For example, "Hello World" is a string. A less intuitive string is something like "6". While it looks like a number to you, python cannot use this for math. "6" + 2 makes no sense to python.
+# Floating point integers are simply integers but with decimal points.
